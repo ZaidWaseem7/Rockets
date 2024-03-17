7ab73a24954600b792eab4ddb9be35e11903c15b
unknown <zaidwaseem9371@gamil.com>
2024-03-17 23:18:41 +0500

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Rocket/*.html","./Rocket/js/*.js"],
+  content: ["./*.html","./js/*.js"],
   theme: {
     extend: {
       screens:{
